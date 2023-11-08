9db91d6972c889ab1478a1ff0738e9045548fe20
Bellamalwa <duchybella@gmail.com>
2023-11-08 05:37:11 -0800

    returns a list containing the titles

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+"""
+Query Reddit API for titles of top ten posts of a given subreddit
+NOTE: Get user agent:
+            https://stackoverflow.com/questions/10606133/ -->
+            sending-user-agent-using-requests-library-in-python
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Return top ten titles for a given subreddit
+    Return None if invalid subreddit given
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Bellamalwa)"
+    }
+    params = {
+        "limit": 10
+        }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,37 @@
+"""
+Query Reddit API recursively for all hot articles of a given subreddit
+NOTE: Get user agent:
+            https://stackoverflow.com/questions/10606133/ -->
+            sending-user-agent-using-requests-library-in-python
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """
+    Return all hot articles for a given subreddit
+    Return None if invalid subreddit given
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Bellamalwa)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
